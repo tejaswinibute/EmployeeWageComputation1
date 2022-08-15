dd381d1c09db87d2376c4eeb9b24c024a4b94b28
tejaswinibute <tejaswinibute31@gmail.com>
2022-08-15 15:48:12 +0530

    [Tejaswini Bute] Add Employee Present Absent

---
+++ EmployeeWage1/EmployeeWage1/EmployeePresentAbsent.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeWage1
+{
+    internal class EmployeePresentAbsent
+    {
+        public void PresentAbsent()
+        {
+            //Class name object name = new (keyword) Classname();
+            Random EmpCheck = new Random();
+            int Check = EmpCheck.Next(0, 2);
+            int EmployeePresent = 1;
+
+            if (Check == EmployeePresent)
+            {
+                Console.WriteLine("Employee is Present");
+            }
+            else
+            {
+                Console.WriteLine("Employee is Absent");
+            }
+        }
+
+    }
+}
---
+++ EmployeeWage1/EmployeeWage1/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace EmployeeWage1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            EmployeePresentAbsent employee = new EmployeePresentAbsent();
+            employee.PresentAbsent();
+
+            DailyWage wage = new DailyWage();
+            wage.DailyWage();
+
+            PartTimeWage PartTimeWage = new PartTimeWage();
+            PartTimeWage.EmployeePartTimeWage();
+
+            SwitchStatement switchwage = new SwitchStatement();
+            switchwage.SwitchDailyWage();
+
+            Console.WriteLine("Total Monthly wage is :- ");
+            MonthWage monthWage = new MonthWage();
+            monthWage.MonthlyWage();
+
+        }
+    }
+}
